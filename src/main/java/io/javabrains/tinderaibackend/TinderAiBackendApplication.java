a5181560fd8497bc93e5589ed349644a8ef2927d
Koushik Kothagal <koush.s@gmail.com>
2024-05-18 14:55:25 -0700

    Basic Initializr app in working state

---
+++ src/main/java/io/javabrains/tinderaibackend/TinderAiBackendApplication.java
@@ -0,0 +1,13 @@
+package io.javabrains.tinderaibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinderAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinderAiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/javabrains/tinderaibackend/TinderAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.tinderaibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinderAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
